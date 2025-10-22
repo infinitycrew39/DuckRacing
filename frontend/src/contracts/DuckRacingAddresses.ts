ee94b7add85cdf3e82c0dba2da55571606021a4d
infinitycrew39 <infinitycrew39@gmail.com>
2025-10-22 09:50:30 +0700

    Add contract information to connect contract

---
+++ frontend/src/contracts/DuckRacingAddresses.ts
@@ -0,0 +1,6 @@
+export const DuckRacingAddresses = { 
+  "11155111": { address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", chainId: 11155111, chainName: "sepolia" },
+  "11155931": { address: "0x1EffB98205B722456F37c0BD07aB169a6E48e29F", chainId: 11155931, chainName: "riseTestnet" },
+  "31337": { address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", chainId: 31337, chainName: "hardhat" },
+  "1337": { address: "0x5FbDB2315678afecb367f032d93F642f64180aa3", chainId: 1337, chainName: "localhost" },
+};
